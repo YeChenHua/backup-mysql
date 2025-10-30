86a0eff9ecadef15398caae46f9516abd5e341bd
YeChenHua <11361118@nfu.edu.tw>
2025-10-30 20:09:00 +0800

    Auto DB backup on 週四 2025/10/30 20:09:00.18

--- backup_learning.bat
+++ backup_learning.bat
@@ -1,23 +1,19 @@
 @echo off
 chcp 65001 >nul
-:: ====== MySQL 資料庫自動備份 ======
+
+:: 1) 匯出資料庫到指定資料夾
 set dbname=learning
 set backup_dir=C:\xampp\mysql\backup-mysql
 set mysql_path=C:\xampp\mysql\bin
+set filename=learning_backup.sql
 
-:: 產生日期時間標籤
-for /f "tokens=1-3 delims=/- " %%a in ('date /t') do set mydate=%%a-%%b-%%c
-for /f "tokens=1-2 delims=: " %%a in ('time /t') do set mytime=%%a%%b
-set filename=%dbname%_backup_%mydate%_%mytime%.sql
-
-:: 匯出資料庫
-echo 正在備份 %dbname% ...
 "%mysql_path%\mysqldump.exe" -u root %dbname% > "%backup_dir%\%filename%"
 
-if %errorlevel%==0 (
-    echo ✅ 備份完成！檔案已儲存為：%filename%
-) else (
-    echo ❌ 備份失敗，請檢查設定！
-)
+:: 2) 推上 GitHub
+cd /d "%backup_dir%"
+git add .
+git commit -m "Auto DB backup on %date% %time%"
+git push
 
+echo ✅ 完成：資料庫已匯出並推送到 GitHub
 pause
